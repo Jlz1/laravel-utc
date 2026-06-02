65e5bc7fcd9ee77f1b6bbb9844c5c65b7540f79e
Jeremy Emmanuel <jeremyes25@gmail.com>
2026-06-02 08:57:38 +0700

    2-1-Bootstrap import, button, column setting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,39 @@
-<h1>
-    This is home
-</h1>
 
 
-<a href="{{route('stocks.index')}}">Go to stocks</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    {{app('Illuminate\Foundation\Vite')
+        (['resources/css/app.css', 'resources/js/app.js']) 
+    }}
+</head>
+<body>
+    <div class="row">
+        <div class="col-md-4">
+            <button type="button" class="btn btn-primary w-100">Primary</button>
+        </div>
+
+        <div class="col-md-1">
+            <a href="{{route('stocks.orders')}}" class="btn btn-secondary w-100">Orders</a>
+        </div>
+
+        <div class="col-md-7">
+            <a href="{{route('stocks.portofolio')}}" class="btn btn-success w-100">Portofolio</a>
+        </div>
+    </div>
+
+
+
+    <br>
+
+    <h1>
+        This is home
+    </h1>
+
+</body>
+</html>
