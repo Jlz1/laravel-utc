0d774c77483a6288a6a9e102dfe5b81d89bb4fc8
Jeremy Emmanuel <jeremyes25@gmail.com>
2026-06-02 11:29:17 +0700

    3-1-create blade template & navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,16 @@
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-    {{app('Illuminate\Foundation\Vite')
-        (['resources/css/app.css', 'resources/js/app.js']) 
-    }}
-</head>
-<body>
-    <div class="row">
-        <div class="col-md-4">
-            <button type="button" class="btn btn-primary w-100">Primary</button>
-        </div>
-
-        <div class="col-md-1">
-            <a href="{{route('stocks.orders')}}" class="btn btn-secondary w-100">Orders</a>
-        </div>
-
-        <div class="col-md-7">
-            <a href="{{route('stocks.portofolio')}}" class="btn btn-success w-100">Portofolio</a>
-        </div>
-    </div>
-
-
-
-    <br>
+@extends('layouts.master')
+@section('title', 'Main Home')
+@section('content')
+    @include('layouts.navbar')
 
     <h1>
         This is home
     </h1>
 
-</body>
-</html>
+    <br><br>
+
+    <div class="row">
+        <div class="col-md-6">
+            <a href="{{route('stocks.index')}}" class="btn btn-primary w-100">Go to Stocks Index</a>
+        </div>
+    </div>
--- resources/views/stocks/stocks_index.blade.php
+++ resources/views/stocks/stocks_index.blade.php
@@ -1,5 +1,18 @@
-<h1>This is Stocks Main Homepage</h1>
+@extends('layouts.master')
+@section('title', 'Stocks Index Page')
+@section('content')
+    @include('layouts.navbar')
 
-<a href="{{route('stocks.portofolio')}}">Go to your portofolio</a>
-<br><br>
-<a href="{{route('stocks.orders')}}">Go to your orders</a>
+    <div class="row">
+        <div class="col-md-4">
+            <button type="button" class="btn btn-primary w-100">Primary</button>
+        </div>
+
+        <div class="col-md-1">
+            <a href="{{route('stocks.orders')}}" class="btn btn-secondary w-100">Orders</a>
+        </div>
+
+        <div class="col-md-7">
+            <a href="{{route('stocks.portofolio')}}" class="btn btn-success w-100">Portofolio</a>
+        </div>
+    </div>
